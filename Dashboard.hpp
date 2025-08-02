40555f97931c17f4ca4155e262eb339ec1cb3ce8
Tanishq Dwivedi <tdwivedi@gmail.com>
2025-08-02 16:16:46 -0700

    need to check imgui documentation.

--- Dashboard.hpp
+++ Dashboard.hpp
@@ -15,6 +15,8 @@ private:
   void SetupROS();
   rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr SOCsub;
   void getSOC(const std_msgs::msg::Float64::SharedPtr msg);
+  void getSOCINT(const std_msgs::msg::Bool::SharedPtr msg);
   float SOC;
   void Controller();
+  std::atomic<bool> SOCint{false};
 };
